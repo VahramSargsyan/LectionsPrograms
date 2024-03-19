2035abe7024969496e33d095eed893e5ae4c371a
VahramSargsyan <vahram85-85@mail.ru>
2024-03-19 17:13:35 +0400

    add some changes

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,7 +1,19 @@
 ﻿int i1 = 6;
 int i2 = 7;
 int sum = i1+i2;
-Console.WriteLine(sum)
+Console.WriteLine(sum);
 
 long l1 = 5;
-Console.WriteLine(l1)
+long sum2 = l1 + i1;
+Console.WriteLine(sum2);
+
+double d1 = 6.7;
+double sum3 = d1 + l1;
+
+
+int product1 = i1 * i2;
+long product2 = l1* i1;
+double product3 = d1 * i1;
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
