e8b984fd8c8bca20cff23cda75d4c3261e65b15c
VahramSargsyan <vahram85-85@mail.ru>
2024-03-19 16:02:10 +0400

    add some variable lesson1:tash_1

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+
+
+int value = 100;
+Console.WriteLine(value);
+double b = 5.5;
+char c ='!';
+bool d = true;
+
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
