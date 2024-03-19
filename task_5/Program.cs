4f46a7d2e479f94c4e3bc6b4daea5703a111e0b4
VahramSargsyan <vahram85-85@mail.ru>
2024-03-19 18:15:21 +0400

    add task_k and did some changes

---
+++ task_5/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int n = 100 ;
+int i = 1  ;
+
+while(i <= n) 
+{
+    // Console.WriteLine(i);
+    Console.Write(i);
+    Console.Write(' ');
+    i = i + 1;
+}
