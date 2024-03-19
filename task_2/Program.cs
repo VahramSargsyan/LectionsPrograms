a2217e60889c94d75efa9e7ca168c5ec2f30d345
VahramSargsyan <vahram85-85@mail.ru>
2024-03-19 17:32:55 +0400

    add  finish changes

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,11 +1,11 @@
 ﻿int i1 = 6;
 int i2 = 7;
 int sum = i1+i2;
-Console.WriteLine(sum);
+// Console.WriteLine(sum);
 
 long l1 = 5;
 long sum2 = l1 + i1;
-Console.WriteLine(sum2);
+// Console.WriteLine(sum2);
 
 double d1 = 6.7;
 double sum3 = d1 + l1;
@@ -14,6 +14,10 @@ double sum3 = d1 + l1;
 int product1 = i1 * i2;
 long product2 = l1* i1;
 double product3 = d1 * i1;
-Console.WriteLine(product1);
-Console.WriteLine(product2);
-Console.WriteLine(product3);
+// Console.WriteLine(product1);
+// Console.WriteLine(product2);
+// Console.WriteLine(product3);
+
+
+double qoutient  = i2 / i1;
+Console.WriteLine(qoutient);
